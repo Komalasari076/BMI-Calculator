cccf3f517c7b83432ba0813e9f277853e74bd554
Komalasari076 <sitikomala076@gmail.com>
2024-12-16 10:53:14 +0700

    update style and logic

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
 function calculateBMI(weight, height) {
-    return weight / Math.pow(height / 100, 2);
+  return weight / Math.pow(height / 100, 2);
+  //menghitung pangkat Math.pow(angka, exponent);
 }
 
-function getBMICategory(bmi) {
-    if (bmi < 18.5) {
-        return'Underweight';
-    } else if (bmi >= 18.5 && bmi <= 24.9) {
-        return 'Normal weight';
-    } else if (bmi >= 25 && bmi <= 29.5) {
-        return 'Overweight';
-    } else if (bmi >= 30){
-        return 'Obesity';
-    } else {
-        return 'entered the input incorrectly';
-    }
+function categoryBMI(bmi) {
+  if (bmi < 18.5) {
+    return "Underweight";
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    return "Normal Weight";
+  } else if (bmi >= 25 && bmi <= 29.5) {
+    return "Overweight";
+  } else if (bmi >= 30) {
+    return "Obesity";
+  } else {
+    return "entered the input incorrectly";
+  }
 }
 
-function buttonClick() {
+function buttonCalculate() {
+  const weight = document.getElementById("weight").value;
+  const height = document.getElementById("height").value;
 
-    const weight = parseFloat(document.getElementById('weight').value);
-    const height = parseFloat(document.getElementById('height').value); 
+  // validasi number
+  if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
+    document.getElementById("result").innerText =
+      "please enter valid weight and height";
+    return;
+  }
 
-    // mengecek validasi number
-    if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
-        document.getElementById('result').innerText = 'Please enter valid weight and height!';
-        return;
-    }
+  const bmi = calculateBMI(weight, height);
+  const categori = categoryBMI(bmi);
 
-    const bmi = calculateBMI(weight, height);
-    const category = getBMICategory(bmi);
-
-    document.getElementById('result').innerText = `Your BMI is ${bmi.toFixed(1)} which means You are ${category}`;
+  document.getElementById("result").innerText = `Your BMI is ${bmi.toFixed(
+    1
+  )} wich means you are ${categori}`;
 }
